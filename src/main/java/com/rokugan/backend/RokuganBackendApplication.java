66d8bea6df1eb036b103040ed5f333a9d3052542
MaxNavarrete <max.e.navarrete@gmail.com>
2025-11-08 20:39:52 -0300

    first commit

---
+++ src/main/java/com/rokugan/backend/RokuganBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rokugan.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokuganBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokuganBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rokugan/backend/RokuganBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rokugan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokuganBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
